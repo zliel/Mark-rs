dff6e8e8a2e3b67fc76a39ebc4b331e3382cd78d
zliel <47030040+zliel@users.noreply.github.com>
2025-07-28 02:00:40 -0700

    Wrapped config errors in the main Error type

--- src/error.rs
+++ src/error.rs
@@ -1,11 +1,13 @@
 use std::fmt;
 use std::io;
 
+use crate::config;
 use crate::thread_pool;
 
 #[derive(Debug)]
 pub enum Error {
     Io(io::Error),
+    Config(config::Error),
     ThreadPool(thread_pool::Error),
 }
 
@@ -13,8 +15,15 @@ impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Error::Io(e) => write!(f, "I/O Error: {e}"),
+            Error::Config(e) => write!(f, "Configuration error: {e}"),
             Error::ThreadPool(e) => write!(f, "Thread pool error: {e}"),
         }
+    }
+}
+
+impl From<config::Error> for Error {
+    fn from(error: config::Error) -> Self {
+        Error::Config(error)
     }
 }
 
