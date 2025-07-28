d4bc1f9c609f7836bfe49567e8828a176da5ab4e
zliel <47030040+zliel@users.noreply.github.com>
2025-07-28 01:51:02 -0700

    Scrapped the old Application-level error type in favor of modular ones

--- src/error.rs
+++ src/error.rs
@@ -2,21 +2,15 @@ use std::fmt;
 use std::io;
 
 #[derive(Debug)]
-pub enum MarkrsError {
-    IO(io::Error),
 }
 
-impl fmt::Display for MarkrsError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            MarkrsError::IO(e) => write!(f, "I/O error: {}", e),
         }
     }
 }
 
-impl From<io::Error> for MarkrsError {
     fn from(error: io::Error) -> Self {
-        MarkrsError::IO(error)
     }
 }
 
