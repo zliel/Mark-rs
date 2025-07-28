a1448b7ce15d676cfe7268449a9455867bac299a
zliel <47030040+zliel@users.noreply.github.com>
2025-07-28 01:51:57 -0700

    Added a wrapper error type to allow propagating up to `run()`, starting with ThreadPool errors

--- src/error.rs
+++ src/error.rs
@@ -1,11 +1,17 @@
 use std::fmt;
 use std::io;
 
+use crate::thread_pool;
+
 #[derive(Debug)]
+pub enum Error {
+    ThreadPool(thread_pool::Error),
 }
 
+impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
+            Error::ThreadPool(e) => write!(f, "Thread pool error: {e}"),
         }
     }
 }
@@ -14,3 +20,8 @@ use std::io;
     }
 }
 
+impl From<thread_pool::Error> for Error {
+    fn from(error: thread_pool::Error) -> Self {
+        Error::ThreadPool(error)
+    }
+}
